f2a73f152dced85f47688ad491cf2786c049116f
ashrubap <ashrubapankhade3@gmail.com>
2024-12-03 00:08:20 +0530

    new change

--- resources/views/website/single_report.blade.php
+++ resources/views/website/single_report.blade.php
@@ -30,66 +30,69 @@
         </div>
         {{-- deport desc --}}
         <div class="flex flex-wrap my-3">
-            <div class="w-1/5">01</div>
             {{-- center div --}}
-            <div class="w-3/5">
+            <div class="w-4/5">
+                <div class="flex justify-center w-full">
+                    <ul class="flex justify-center gap-5 ">
+                        <li class="border shadow-sm hover:shadow-lg hover:bg-blue-600 hover:text-white px-4 py-1">Descriotion</li>
+                        <li class="border shadow-sm hover:shadow-lg hover:bg-blue-600 hover:text-white px-4 py-1">TOC</li>
+                        <li class="border shadow-sm hover:shadow-lg hover:bg-blue-600 hover:text-white px-4 py-1">Methodology</li>
+                        <li class="border shadow-sm hover:shadow-lg hover:bg-blue-600 hover:text-white px-4 py-1">Segmentation</li>
+                        <li class="border shadow-sm hover:shadow-lg hover:bg-blue-600 hover:text-white px-4 py-1">Sample PDF</li>
+                    </ul>
+                </div>
 
-          
 
             </div>
 
             {{-- right side div --}}
             <div class="w-1/5">
-                <div class="card rounded-md shadow-md border-2  h-fit bg-blue-50">
-                    <div class="mt-4 space-y-2">
+                <div class="sticky">
+                    <div class="card rounded-md shadow-md border-2  h-fit  flex flex-col-reverse gap-5 px-5 py-3">
+                        <button
+                            class="py-1 text-md font-semibold border-2 border-blue-400 hover:bg-blue-800 hover:text-white rounded-md hover:shadow-md">Buy
+                            New </button>
+                        <button
+                            class="py-1 text-md font-semibold border-2 border-blue-400 hover:bg-blue-800 hover:text-white rounded-md hover:shadow-md">
+                            Request Sample </button>
+                        <button
+                            class="py-1 text-md font-semibold border-2 border-blue-400 hover:bg-blue-800 hover:text-white rounded-md hover:shadow-md">
+                            Ask For Customization </button>
+                        <button
+                            class="py-1 text-md font-semibold border-2 border-blue-400 hover:bg-blue-800 hover:text-white rounded-md hover:shadow-md">
+                            Get a Quote </button>
+                    </div>
 
-                        <label for="google"
-                            class="flex w-full justify-between text-center text-slate-700 has-[:checked]:ring-indigo-200 has-[:checked]:text-indigo-800 has-[:checked]:bg-indigo-50 items-center gap-6 rounded-lg p-4 ring-1 ring-transparent hover:bg-slate-100">
+                    <div class="card rounded-md shadow-md border-2  h-fit  px-5 py-3 mt-5 justify-between ">
+                        <h2 class="text-lg font-semibold">
+                            Multi-report Purchase Plan
+                        </h2>
 
-                            Single License
-                            <input name="payment_method" id="google" value="apple" type="radio"
-                                class="box-content h-1.5 w-1.5 appearance-none rounded-full border-[5px] border-white bg-white bg-clip-padding outline-none ring-1 ring-gray-950/10 checked:border-indigo-500 checked:ring-indigo-500">
+                        <p> A Customized Plan Will be Created Based on the number of reports you wish to purchase </p>
+                        <a href="#">
+                            <button type="button"
+                                class="text-md w-full m-2 py-1 font-semibold border-2 border-blue-400 hover:bg-blue-800 hover:text-white rounded-md hover:shadow-md">Inquire
+                            </button>
+                        </a>
 
-                        </label>
-
-                        <label for="credit-card"
-                            class="flex w-full justify-between text-center text-slate-700 has-[:checked]:ring-indigo-200 has-[:checked]:text-indigo-800 has-[:checked]:bg-indigo-50 items-center gap-6 rounded-lg p-4 ring-1 ring-transparent hover:bg-slate-100">
-
-                            Team License
-                            <input name="payment_method" id="credit-card" value="credit-card" type="radio"
-                                class="box-content h-1.5 w-1.5 appearance-none rounded-full border-[5px] border-white bg-white bg-clip-padding outline-none ring-1 ring-gray-950/10 checked:border-indigo-500 checked:ring-indigo-500">
-                        </label>
-
-                        <label for="corporate"
-                            class="flex w-full justify-between text-center text-slate-700 has-[:checked]:ring-indigo-200 has-[:checked]:text-indigo-800 has-[:checked]:bg-indigo-50 items-center gap-6 rounded-lg p-4 ring-1 ring-transparent hover:bg-slate-100">
-
-                            Enterprise/Corporate License
-                            <input name="payment_method" id="corporate" value="credit-card" type="radio"
-                                class="box-content h-1.5 w-1.5 appearance-none rounded-full border-[5px] border-white bg-white bg-clip-padding outline-none ring-1 ring-gray-950/10 checked:border-indigo-500 checked:ring-indigo-500">
-                        </label>
-                        <label for="platform"
-                            class="flex w-full justify-between text-center text-slate-700 has-[:checked]:ring-indigo-200 has-[:checked]:text-indigo-800 has-[:checked]:bg-indigo-50 items-center gap-6 rounded-lg p-4 ring-1 ring-transparent hover:bg-slate-100">
-
-                            Platform Access
-                            <input name="payment_method" id="platform" value="credit-card" type="radio"
-                                class="box-content h-1.5 w-1.5 appearance-none rounded-full border-[5px] border-white bg-white bg-clip-padding outline-none ring-1 ring-gray-950/10 checked:border-indigo-500 checked:ring-indigo-500">
-                        </label>
                     </div>
                 </div>
+
             </div>
 
         </div>
 
         {{-- releted reports --}}
         <div>
-            <h3 class=" text-center justify-between  mt-10 py-5 text-gray-600 text-3xl max-tablet:text-2xl font-bold">Releted reports</h3>
+            <h3 class=" text-center justify-between  mt-10 py-5 text-gray-600 text-3xl max-tablet:text-2xl font-bold">
+                Releted reports</h3>
         </div>
         <div class="flex justify-between gap-3 mb-16 mx-5">
-            <x-releted-reports-card/>
-            <x-releted-reports-card/>
-            <x-releted-reports-card/>
-            <x-releted-reports-card/>
-            <x-releted-reports-card/>
+            <x-releted-reports-card />
+            <x-releted-reports-card />
+            <x-releted-reports-card />
+            <x-releted-reports-card />
+            <x-releted-reports-card />
 
         </div>
     </div>
